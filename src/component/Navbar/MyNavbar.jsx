22b5f16d768047c9a6d93119fef158867a8318eb
omarmokhtar <you@example.com>
2024-03-27 23:38:12 +0200

    third commit68

--- src/component/Navbar/MyNavbar.jsx
+++ src/component/Navbar/MyNavbar.jsx
@@ -23,7 +23,7 @@ function MyNavbar() {
               aria-label="Search"
             />
           </Form>
-          <Nav className="me-auto" style={{ alignItems: 'baseline',fontFamily:'Cairo' }}>
+          {/* <Nav className="me-auto" style={{ alignItems: 'baseline',fontFamily:'Cairo' }}>
           <NavDropdown title="عربي" id="collapsible-nav-dropdown" style={{ width: '105px', border: '1px solid #547AFF', height: '40px', borderRadius: '10px',display:'flex',justifyContent:'center' }}>
               <NavDropdown.Item href="#action/3.1">عربي</NavDropdown.Item>
             </NavDropdown>
@@ -31,7 +31,7 @@ function MyNavbar() {
               <Button style={{ backgroundColor: '#547AFF', width: '105px', height: '40px', borderRadius: '10px' }}>كيفيه العمل</Button>
             </Nav.Link>
             
-          </Nav>
+          </Nav> */}
           
 
         </Navbar.Collapse>
